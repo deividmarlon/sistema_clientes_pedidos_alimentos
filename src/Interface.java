af1e0ea20ed4b3cce665b0b436f95d1071f6d37b
danielzontaojeda <danielzontaojeda@gmail.com>
2022-03-12 19:50:55 -0300

    created interface class

--- src/Interface.java
+++ src/Interface.java
@@ -1,7 +1,159 @@
+import java.util.Scanner;
+
 public class Interface {
 
+    //constant with the software name to calculate size of some menus. needs to be even sized.
+    private static final String softwareName = "Food orders management system - Main Menu:";
+
+    //prints on screen a header with the software name and authors' name
+    //pre-conditions: none
+    //post-conditions: header printed on screen
     public void startScreen(){
-        for(int i = 0; i < 60;i++) System.out.println("*");
-        System.out.println("\n");
+        int i;
+        int messageSize = softwareName.length();
+
+        for(i = 0; i < 100;i++) System.out.print("*");
+        System.out.println();
+
+        for(i = 0; i < 10; i++) System.out.print("*");
+        for(i = 0; i < ((80-messageSize)/2); i++) System.out.print(" ");
+        System.out.print("Food orders management system.");
+        for(i = 0; i < ((80-messageSize)/2); i++) System.out.print(" ");
+        for(i = 0; i < 10; i++) System.out.print("*");
+        System.out.println();
+
+        for(int j = 0; j < 2; j++) {
+            for (i = 0; i < 10; i++) System.out.print("*");
+            for (i = 0; i < 80; i++) System.out.print(" ");
+            for (i = 0; i < 10; i++) System.out.print("*");
+            System.out.println();
+        }
+        for (i = 0; i < 10; i++) System.out.print("*");
+        for (i = 0; i < 19; i++) System.out.print(" ");
+        System.out.print("Authors: Daniel Zonta Ojeda, Deivid Marlon");
+        for (i = 0; i < 19; i++) System.out.print(" ");
+        for (i = 0; i < 10; i++) System.out.print("*");
+        System.out.println();
+        for(i = 0; i < 100;i++) System.out.print("*");
+        System.out.println();
+    }
+
+    //prints main menu on screen and returns user choice
+    //pre-conditions: none
+    //post-conditions: main menu printed on screen until a valid user choice
+    //then returns user choice
+    public int mainMenu(){
+        int i;
+        int messageSize = softwareName.length();
+        Scanner scan = new Scanner(System.in);
+        int choice = 0;
+        boolean valid = false;
+
+        for(i = 0; i < ((100-messageSize)/2); i++) System.out.print(" ");
+        System.out.print(softwareName);
+        for(i = 0; i < ((100-messageSize)/2); i++) System.out.print(" ");
+        System.out.println();
+
+        do{
+        System.out.println();
+        System.out.println("1. Client Menu.");
+        System.out.println("2. Food Menu.");
+        System.out.println("3. Report Menu.");
+        System.out.println("4. Exit program.");
+
+        //checks if it's int
+        if (scan.hasNextInt()) {
+            choice = scan.nextInt();
+            //checks if is a valid input
+            if(choice>=1 && choice<=4 )valid = true;
+            else{
+                System.out.println("ERROR: Invalid input.");
+                scan.nextLine();
+            }
+        }else{
+            System.out.println("ERROR: Invalid input.");
+            scan.nextLine();
+        }
+    }while(!valid);
+
+        if(choice != 4) return choice;
+        else return 0;
+    }
+
+    //prints client menu, reads an int and returns it if it's valid
+    //pre-conditions: none
+    //post-conditions: returns a valid user choice. will return 0 in the case
+    //to go back to main menu
+    public int clientMenu() {
+        Scanner scan = new Scanner(System.in);
+        int choice = 0;
+        boolean valid = false;
+
+        //will repeat until valid input
+        do {
+            System.out.println();
+            System.out.println("1. Register new client.");
+            System.out.println("2. Edit existing client.");
+            System.out.println("3. Remove existing client.");
+            System.out.println("4. Find client.");
+            System.out.println("5. View history.");
+            System.out.println("6. View full history.");
+            System.out.println("7. Exit to main menu");
+
+            //checks if it's int
+            if (scan.hasNextInt()) {
+                choice = scan.nextInt();
+                //checks if is a valid input
+                if(choice>=1 && choice<=7 )valid = true;
+                else{
+                    System.out.println("ERROR: Invalid input.");
+                    scan.nextLine();
+                }
+            }else{
+                System.out.println("ERROR: Invalid input.");
+                scan.nextLine();
+            }
+        }while(!valid);
+
+        if(choice != 7) return choice;
+        else return 0;
+    }
+    //prints food menu, reads an int and returns it if it's valid
+    //pre-conditions: none
+    //post-conditions: returns a valid user choice. will return 0 in the case
+    //to go back to main menu
+    public int foodMenu(){
+        Scanner scan = new Scanner(System.in);
+        int choice = 0;
+        boolean valid = false;
+
+        //will repeat until valid input
+        do {
+            System.out.println();
+            System.out.println("1. Register new food.");
+            System.out.println("2. Edit existing food.");
+            System.out.println("3. Remove existing food.");
+            System.out.println("4. Find food.");
+            System.out.println("5. List food");
+            System.out.println("6. Exit to main menu");
+
+            //checks if it's int
+            if (scan.hasNextInt()) {
+                choice = scan.nextInt();
+                //checks if is a valid input
+                if(choice>=1 && choice<=6 )valid = true;
+                else{
+                    System.out.println("ERROR: Invalid input.");
+                    scan.nextLine();
+                }
+            }else{
+                System.out.println("ERROR: Invalid input.");
+                scan.nextLine();
+            }
+        }while(!valid);
+
+        if(choice != 6) return choice;
+        else return 0;
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+
+        int choiceMainMenu, choiceSubMenu;
+
+        Interface screen = new Interface();
+
+        //prints header on screen
+        screen.startScreen();
+
+        //prints main menu on screen and gets user choice
+        //repeats until user chooses to exit program
+        do {
+            choiceMainMenu = screen.mainMenu();
+
+            switch (choiceMainMenu) {
+                case 1:
+                    choiceSubMenu = screen.clientMenu();
+                    if (choiceSubMenu != 0) {
+                        //TODO create client class
+                        //Client client = new Client();
+                        //client.configClient(choiceSubMenu);
+                    }
+                    break;
+                case 2:
+                    choiceSubMenu = screen.foodMenu();
+                    if (choiceSubMenu != 0) {
+                        //TODO create food class
+                        //Food food = new Food();
+                        //food.configFood(choiceSubMenu);
+                    }
+                    break;
+                case 3:
+                    //TODO report interface menu
+                    break;
+            }
+        }while(choiceMainMenu != 0);
+        System.out.println("Thank you for using our software!");
     }
 }
